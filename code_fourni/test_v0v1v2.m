c34672484ca699546cdf875a6d8c06b89bd32302
Antoine2803 <82040673+Antoine2803@users.noreply.github.com>
2024-05-02 08:49:01 +0200

    tests de v0v1v2 corrigés

--- code_fourni/test_v0v1v2.m
+++ code_fourni/test_v0v1v2.m
@@ -6,7 +6,7 @@ clc
 %%%%%%%%%%%%
 
 % taille de la matrice symétrique
-for n = 600:200:600
+for n = 200:200:200
 
 % type de la matrice (voir matgen_csad)
 % imat == 1 valeurs propres D(i) = i
@@ -14,7 +14,7 @@ for n = 600:200:600
 %                                  uniformément répartie, cond = 1e10
 % imat == 3 valeurs propres D(i) = cond**(-(i-1)/(n-1)) avec cond = 1e5
 % imat == 4 valeurs propres D(i) = 1 - ((i-1)/(n-1))*(1 - 1/cond) avec cond = 1e2
-for imat = 2:3
+for imat = 1:4
 
 % tolérance
 eps = 1e-8;
@@ -22,7 +22,7 @@ eps = 1e-8;
 maxit = 10000;
 
 % on génère la matrice (1) ou on lit dans un fichier (0)
-genere = 1;
+genere = 0;
 
 % méthode de calcul
 v = 0; % subspace iteration v0
@@ -30,34 +30,28 @@ v = 0; % subspace iteration v0
 % nombre de valeurs propres cherchées (v0)
 m = 20;
 
-%[W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, [], [], genere);
-%fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
-%fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+[W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, [], [], genere);
+fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
+fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
 
 % méthode de calcul
 v = 1;
 
-% pourcentage de la trace que l'on veut atteindre (v1, v2, v3)
-%percentage = .1;
+% pourcentage de la trace que l'on veut atteindre (v1, v2)
+percentage = .1;
 
-%genere = 0;
-%[W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, percentage, [], genere);
-%fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
-%fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
+[W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, percentage, [], genere);
+fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
+fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
 
 % méthode de calcul
 v = 2;
 
-% pourcentage de la trace que l'on veut atteindre (v1, v2, v3)
-percentage = .1;
-
-genere = 0;
-for p = 2:2:10
-    [W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, percentage, p, genere);
-    fprintf("---- p=%d \n",p);
-    fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
-    fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
-end
+p = 8; %8 etant la meilleure valeur obtenue experimentalement
+[W, V, flag, q, qv] = eigen_2024(imat, n, v, m, eps, maxit, percentage, p, genere);
+fprintf("---- p=%d \n",p);
+fprintf('Qualité des couples propres (par rapport au critère d''arrêt) = [%0.3e , %0.3e]\n', min(qv), max(qv));
+fprintf('Qualité des valeurs propres (par rapport au spectre de la matrice) = [%0.3e , %0.3e] \n', min(q), max(q));
 
 end
 end
